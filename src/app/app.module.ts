d924895ac2fbd0e81a03ee7ecc87f01760c5fc09
JordanMac <fraryjordan000@gmail.com>
2019-01-22 09:09:16 -0700

    added needed pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeadlinesComponent } from './headlines/headlines.component';
+import { SearchComponent } from './search/search.component';
+import { LikesComponent } from './likes/likes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeadlinesComponent,
+    SearchComponent,
+    LikesComponent
   ],
   imports: [
     BrowserModule,
