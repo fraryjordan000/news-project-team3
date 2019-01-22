39ae225e937051a5150aabe9fb14593cf9045be7
JordanMac <fraryjordan000@gmail.com>
2019-01-22 09:59:08 -0700

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HeadlinesComponent } from './headlines/headlines.component';
+import { SearchComponent } from './search/search.component';
+import { LikesComponent } from './likes/likes.component';
+import { LoginComponent } from './login/login.component';
+import { AuthService } from './auth.service';
+
+const routes: Routes = [
+  { path: 'headlines', component: HeadlinesComponent, canActivate: [AuthService] },
+  { path: 'search', component: SearchComponent, canActivate: [AuthService] },
+  { path: 'likes', component: LikesComponent, canActivate: [AuthService] },
+  { path: 'login', component: LoginComponent, canActivate: [AuthService] },
+  { path: '', redirectTo: 'login' },
+  { path: '**', redirectTo: 'login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
