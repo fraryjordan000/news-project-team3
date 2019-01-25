9a8af8646e4f76b9e2f4e59f3a2b2ef000c33cba
JordanMac <fraryjordan000@gmail.com>
2019-01-25 09:59:31 -0700

    article component complete

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiFetchService } from '../api-fetch.service';
 
+import { ArticleComponent } from '../shared/article/article.component';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-search',
@@ -11,8 +13,14 @@ export class SearchComponent implements OnInit {
 
   // In html, *ngFor="let card in cards" -- use card.title, card.description, card.url, and card.urlToImage
   cards: any = [];
+  card = {
+    url: "https://google.com",
+    urlToImage: this.sanitizer.bypassSecurityTrustUrl("https://media.pitchfork.com/photos/5c49d4dea8b1520c39cc9b4d/1:1/w_500/Better-Oblivion-Community-Center.jpg"),
+    title: "Better oblivion review",
+    description: "like a soap opera of modern politics"
+  }
 
-  constructor(private fetch: ApiFetchService) { }
+  constructor(private fetch: ApiFetchService, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
   }
--- src/app/shared/article/article.component.ts
+++ src/app/shared/article/article.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss']
 })
+
 export class ArticleComponent implements OnInit {
+
+  @Input() card;
+
+  selected: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  clicked() {
+    this.selected = !this.selected;
   }
 
 }
