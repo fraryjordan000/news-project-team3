88c492d1676a99993d3389289dc2ca7ea5248eb8
Blake M <Blake.manookin@gmail.com>
2019-02-01 09:49:04 -0700

    Navbar icon responsive

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import {BreakpointObserver, Breakpoints, BreakpointState} from '@angular/cdk/layout';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
+
 export class NavbarComponent implements OnInit {
 
   icon: boolean = false;
 
-  constructor(public auth: AuthService, private router: Router) { }
+
+  constructor(public auth: AuthService, private router: Router, public breakpointobserver: BreakpointObserver) {
+  }
 
   ngOnInit() {
+    this.breakpointobserver
+      .observe([Breakpoints.Small, Breakpoints.HandsetPortrait])
+      .subscribe((state: BreakpointState) => {
+        this.icon = state.matches;
+
+      });
   }
 
   logout() {
@@ -21,5 +32,6 @@ export class NavbarComponent implements OnInit {
       this.router.navigate(['/login']);
     });
   }
+
 
 }
